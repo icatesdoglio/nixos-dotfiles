46ac8b79c0fa5eaf5db48df960c5ceea7ed9340a
Ian Cates-Doglio <icates-doglio@teainc.org>
2026-01-17 22:27:01 -0800

    updates to servemato mostly

--- hosts/ServeMato/default.nix
+++ hosts/ServeMato/default.nix
@@ -50,9 +50,17 @@
           group = "root";
           mode = "0400";
       };
+      "qbtPassword-encrypted" = {
+          sopsFile = ../../secrets/qbt.yaml;
+          format = "yaml";
+          path = "/run/secrets/qbtPassword";
+          owner = "qbit";
+          group = "qbit";
+          mode = "0400";
+      };
   };
 
-  environment.systemPackages = with pkgs; [ age sops ssh-to-age ];
+  environment.systemPackages = with pkgs; [ age sops ssh-to-age iproute2 ];
 
   users.users.root.openssh.authorizedKeys.keys = [
       "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIBtghfDdBX+s+XnZbnP3kiV83dCVymSO4Zhv/SudP8pS"
@@ -84,9 +92,26 @@
    ***********************************/
   my.networking.unbound.enable = true;
   my.networking.pihole.enable = true;
-  networking.firewall.allowedUDPPorts = [ 51820 ];
-  networking.firewall.interfaces.wg0.allowedUDPPorts = [ 53 51820 ];
-  networking.firewall.interfaces.wg0.allowedTCPPorts = [ 53 ];
+  networking.firewall.allowedUDPPorts = [ 
+      51820 # Wireguard
+  ];
+  networking.firewall.interfaces.wg0.allowedUDPPorts = [ 
+      53      # DNS
+      51820   # Wireguard
+      51821   # Torrent port (UDP, DHT)
+  ];
+  networking.firewall.interfaces.wg0.allowedTCPPorts = [ 
+      53      # DNS
+      3000    # Home Page
+      8081    # Web UI
+      51821   # Torrent port (TCP)
+  ];
+  networking.firewall.interfaces."ca-van".allowedUDPPorts = [ 
+      51821   # Torrent port (UDP, DHT)
+  ];
+  networking.firewall.interfaces."ca-van".allowedTCPPorts = [ 
+      51821   # Torrent port (TCP)
+  ];
 
   /*************
     VPN Settings
@@ -126,16 +151,29 @@
                   allowedIPs = [ "10.100.0.5/32" ];
                   persistentKeepalive = 25;
               };
-	      framework = {
-		      publicKey = "8A7L4okGuJSPtHIHxVNcTT18iGKr50Ipz18G9LAQKgE=";
-		      allowedIPs = [ "10.100.0.6/32" ];
-		      persistentKeepalive = 25;
+              framework = {
+                  publicKey = "8A7L4okGuJSPtHIHxVNcTT18iGKr50Ipz18G9LAQKgE=";
+                  allowedIPs = [ "10.100.0.6/32" ];
+                  persistentKeepalive = 25;
 
-	      };
+              };
           };
       };
-
   };
+
+  networking.wireguard.interfaces.ca-van = {
+      ips = [ "10.14.0.2/16" ];
+
+      privateKeyFile = config.sops.secrets."wg/surfshark/servemato".path;
+
+      peers = [{
+          publicKey = "o4HezxSsbNqJFJZj+VBw/QXFLpfNo7PZu8xe7H2hTw0=";
+          endpoint = "ca-van.prod.surfshark.com:51820";
+          allowedIPs = [ "0.0.0.0/0" ];
+          persistentKeepalive = 25;
+      }];
+  };
+
 
   /*** ensure domain in cloudflare has correct configuration" ***/
   services.cloudflare-dyndns = {
@@ -147,18 +185,101 @@
       proxied = false;
   };
 
-  services.qbittorrent = {
+  /*********************
+   *** SERVER CONFIG ***
+   *********************/
+
+  /* landing page */
+  services.homepage-dashboard = {
       enable = true;
-      openWebUI = true;
-      dataDir = "/mnt/data/torrents";
-      user = "qbit";
-      openWebUI = {
-          enable = true;
-          port = 8728;
+
+      listenPort = 3000;
+
+      settings = {
+          title = "ServeMato";
+          theme = "dark";
+          color = "slate";
       };
-      networkNamespace = "vpn";
   };
 
+
+
+
+  services.qbittorrent = {
+      enable = true;
+      user = "qbit";
+      group = "qbit";
+
+      webuiPort = 8081;
+      torrentingPort = 51821;
+
+      openFirewall = false; # More restrictive setup
+
+          serverConfig = {
+              /* Download paths */
+              Downloads.SavePath = "/mnt/data/torrents";
+              Downloads.TempPath = "/mnt/data/torrents/incomplete";
+              Downloads.TempPathEnabled = true;
+
+              Preferences.WebUI.Username = "admin"; 
+              Preferences.WebUI.Password_PBKDF2 = {
+                  _secret = config.sops.secrets.qbtPassword-encrypted.path;
+              };
+              /* Security / sanity */
+              Preferences.WebUI.LocalHostAuth = false;
+              Preferences.WebUI.AuthSubnetWhitelistEnabled = true;
+              Preferences.WebUI.AuthSubnetWhitelist = "10.100.0.0/24";
+
+              /* Optional: don’t expose UPnP */
+              Preferences.Connection.UPnP = false;
+          };
+  };
+
+  environment.etc."iproute2/rt_tables".text = ''
+      200 vpn
+      '';
+
+  systemd.services.qbittorrent-policy-routing = {
+      description = "Policy routing for qBittorrent over WireGuard";
+      wantedBy = [ "multi-user.target" ];
+      after = [
+          "network-online.target"
+              "wireguard-ca-van.service"
+              "sys-subsystem-net-devices-ca-van.device"
+      ];
+      wants = [ 
+          "network-online.target" 
+          "sys-subsystem-net-devices-ca-van.device"
+      ];
+
+      serviceConfig = {
+          Type = "oneshot";
+          RemainAfterExit = true;
+
+          execStop = let
+              qbitUid = toString config.users.users.qbit.uid;
+          ip = "${pkgs.iproute2}/bin/ip";
+          in ''
+              ${ip} rule del to 10.100.0.0/24 lookup main || true
+              ${ip} rule del uidrange ${qbitUid}-${qbitUid} lookup vpn || true
+              '';
+      };
+
+      script = let
+          qbitUid = toString config.users.users.qbit.uid;
+      ip = "${pkgs.iproute2}/bin/ip";
+      in ''
+# Ensure wg admin traffic never goes to VPN
+          ${ip} rule add to 10.100.0.0/24 lookup main priority 9000 || true
+
+# Force qbit traffic to VPN
+          ${ip} route replace default dev ca-van table vpn
+          ${ip} rule add uidrange ${qbitUid}-${qbitUid} lookup vpn priority 10000 || true
+
+          ${ip} route flush cache
+          '';
+
+  };
 
   zramSwap.enable = true;
 
