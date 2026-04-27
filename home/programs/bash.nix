c32b4abac81765a9cfadfc78663e2b0c838a9a4d
Ian Cates-Doglio <iancatesdoglio@gmail.com>
2026-04-27 11:02:23 -0700

    update nixpkgs

--- home/programs/bash.nix
+++ home/programs/bash.nix
@@ -9,10 +9,15 @@
     shellOptions = ["histappend" "extglob" "globstar" "checkjobs"];
 
     initExtra = ''
-      _git_branch() {
-        git branch --show-current 2>/dev/null | sed 's/\(.*\)/(\1)/'
-      }
-      PS1='\[\e[38;2;122;162;247m\][\u@\h \w]\[\e[38;2;194;139;44m\]$(_git_branch)\[\e[38;2;158;206;106m\]\$\[\e[0m\] '
-    '';
+        _prompt_git_branch() {
+            local branch
+                branch="$(git branch --show-current 2>/dev/null)" || return
+                [[ -z "$branch" ]] && return
+
+                printf '(%s)' "$branch"
+        }
+
+    PS1='\[\e[38;2;122;162;247m\][\u@\h \w]\[\e[38;2;194;139;44m\]$(_prompt_git_branch)\[\e[38;2;158;206;106m\]\$\[\e[0m\] '
+        '';
   };
 }
